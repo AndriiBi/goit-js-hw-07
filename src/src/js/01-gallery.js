4941b90e05056b12bde64f351bf4175c59142765
AndriiBi <137702759+AndriiBi@users.noreply.github.com>
2023-11-11 13:14:24 +0100

    task 2

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector(".gallery");
 
-// Function to create a gallery item
+// Функція для створення "li", "a", "img" у списку "gallery". 
 function createGalleryItem(item) {
   const listItem = document.createElement("li");
   listItem.classList.add("gallery__item");
@@ -20,29 +20,23 @@ function createGalleryItem(item) {
   link.appendChild(image);
   listItem.appendChild(link);
 
- // Add a click event listener to each gallery item
+ // EventListener для кожного "li" при кліку
  listItem.addEventListener('click', (event) => {
     event.preventDefault();
 
-    // Create a Lightbox instance with the large image source
-    const lightbox = basicLightbox.create(`
-      <img src="${item.original}" width="800" height="600">
-    `);
+ const lightbox = basicLightbox.create(`<img src="${item.original}" width="800" height="600">`);
 
-    // Show the Lightbox
     lightbox.show();
   });
 
   return listItem;
 }
 
-// Function to append gallery items to the list
+// Апенд зображень у список
 function appendGalleryItems(items) {
   items.forEach(item => {
     const galleryItem = createGalleryItem(item);
     galleryList.appendChild(galleryItem);
   });
 }
-
-// Call the function to append gallery items to the list
 appendGalleryItems(galleryItems);
--- src/src/js/02-lightbox.js
+++ src/src/js/02-lightbox.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+// Функція для створення "li", "a", "img" у списку "gallery".
+function createGalleryItem(item) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
+
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
+
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  //   image.dataset.source = item.original;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+  // EventListener для кожного "li" при кліку
+  listItem.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const lightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionPosition: "bottom",
+      captionDelay: 250
+    });
+
+    lightbox.open();
+  });
+
+  return listItem;
+}
+
+// Апенд зображень у список
+function appendGalleryItems(items) {
+  items.forEach((item) => {
+    const galleryItem = createGalleryItem(item);
+    galleryList.appendChild(galleryItem);
+  });
+}
+appendGalleryItems(galleryItems);
