720d5d9f2c1c4ec54e918b62c43cb7dfc3de94a0
AndriiBi <137702759+AndriiBi@users.noreply.github.com>
2023-11-11 12:40:42 +0100

    task 1

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,16 +1,48 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
 const galleryList = document.querySelector(".gallery");
 
-galleryItems.forEach(item => {
-    const listItem = document.createElement("li");
-    const image = document.createElement("img");
+// Function to create a gallery item
+function createGalleryItem(item) {
+  const listItem = document.createElement("li");
+  listItem.classList.add("gallery__item");
 
-    image.src = item.preview;
-    image.alt = item.description;
+  const link = document.createElement("a");
+  link.classList.add("gallery__link");
+  link.href = item.original;
 
-    listItem.appendChild(image);
-    galleryList.appendChild(listItem);
-});
+  const image = document.createElement("img");
+  image.classList.add("gallery__image");
+  image.src = item.preview;
+  image.dataset.source = item.original;
+  image.alt = item.description;
 
-console.log(galleryItems);
+  link.appendChild(image);
+  listItem.appendChild(link);
+
+ // Add a click event listener to each gallery item
+ listItem.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    // Create a Lightbox instance with the large image source
+    const lightbox = basicLightbox.create(`
+      <img src="${item.original}" width="800" height="600">
+    `);
+
+    // Show the Lightbox
+    lightbox.show();
+  });
+
+  return listItem;
+}
+
+// Function to append gallery items to the list
+function appendGalleryItems(items) {
+  items.forEach(item => {
+    const galleryItem = createGalleryItem(item);
+    galleryList.appendChild(galleryItem);
+  });
+}
+
+// Call the function to append gallery items to the list
+appendGalleryItems(galleryItems);
